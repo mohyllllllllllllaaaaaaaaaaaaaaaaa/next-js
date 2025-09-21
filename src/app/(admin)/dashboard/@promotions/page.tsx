b2c314e75de4c03ccdeab2bb837ffa28ec656a91
natalia mohyla <mogilesha81@gmail.com>
2025-09-21 17:53:00 +0200

    add fetch

--- src/app/(admin)/dashboard/@promotions/page.tsx
+++ src/app/(admin)/dashboard/@promotions/page.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
-import { getSummaryPromotions } from '@/lib/api';
 import SummaryTable from '@/app/components/summary-table';
 import SummaryTableHeader from '@/app/components/summary-table-header';
 import SummaryTableCell from '@/app/components/summary-table-cell';
 import DashboardCard from '@/app/components/dash-board-card';
+import {getPromotions} from "@/lib/api";
 
 export interface PageProps {}
 
 export default async function Page({}: PageProps) {
-    const data = await getSummaryPromotions();
+    const data = await getPromotions();
+
 
     return (
         <DashboardCard label="Promotions">
@@ -21,14 +22,17 @@ export default async function Page({}: PageProps) {
                     </>
                 }
             >
-                {data.map(({ promotionId, promotionName, companyTitle, discount }) => (
-                    <tr key={promotionId}>
+
+                {data.map(({ id, title, companyTitle, discount }) => (
+                    <tr key={id}>
+
                         <SummaryTableCell>{companyTitle}</SummaryTableCell>
-                        <SummaryTableCell>{promotionName}</SummaryTableCell>
+                        <SummaryTableCell>{title}</SummaryTableCell>
                         <SummaryTableCell align="center">{`-${discount}%`}</SummaryTableCell>
                     </tr>
                 ))}
             </SummaryTable>
+
         </DashboardCard>
     );
 }
