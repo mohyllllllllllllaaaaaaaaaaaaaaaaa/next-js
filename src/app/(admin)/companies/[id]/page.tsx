bcd8a1f1c38ea93f8550103b261578969fddb76c
natalia mohyla <mogilesha81@gmail.com>
2025-09-22 18:14:41 +0200

    add client

--- src/app/(admin)/companies/[id]/page.tsx
+++ src/app/(admin)/companies/[id]/page.tsx
@@ -1,7 +1,11 @@
 'use client';
 
-import React, {useEffect} from 'react';
-import Header from "@/app/components/header";
+import React  from 'react';
+import { dehydrate, HydrationBoundary } from '@tanstack/react-query';
+import { Company, getCompany, getPromotions } from '@/lib/api';
+import getQueryClient from '@/lib/utils/getQueryClient';
+import CompanyInfo from '@/app/components/company-info';
+import CompanyPromotions from '@/app/components/company-promotions';
 import {notFound} from "next/navigation";
 
 
@@ -9,17 +13,39 @@ export interface PageProps {
     params: {id: string};
 }
 
-function Page({params}: PageProps) {
-    useEffect(() => {
-        const id = Number.parseInt(params.id);
-        if(Number.isNaN(id)){
-            notFound();
-        }
-    }, [params.id]);
+async function Page({params}: PageProps) {
+    const queryClient = getQueryClient();
+
+    await queryClient.prefetchQuery({
+        queryKey: ['companies', params.id],
+        queryFn: () => getCompany(params.id, { cache: 'no-store' }),
+        staleTime: 10 * 1000,
+    });
+
+    await queryClient.prefetchQuery({
+        queryKey: ['promotions', params.id],
+        queryFn: () =>
+            getPromotions({ companyId: params.id }, { cache: 'no-store' }),
+        staleTime: 10 * 1000,
+    });
+
+    const company = queryClient.getQueryData(['companies', params.id]) as Company;
+    if (!company) {
+        notFound();
+    }
+
+    const dehydratedState = dehydrate(queryClient);
     return (
-        <div className="py-6 px-10">
-            <p>{`Information about company (${params.id})`}</p>
-        </div>
+        <HydrationBoundary state={dehydratedState}>
+            <div className="py-6 px-10 grid grid-cols-12 gap-5">
+                <div className="col-span-3">
+                    <CompanyInfo companyId={params.id} />
+                </div>
+                <div className="col-span-9">
+                    <CompanyPromotions companyId={params.id} />
+                </div>
+            </div>
+        </HydrationBoundary>
     );
 }
 
--- src/app/components/company-table.tsx
+++ src/app/components/company-table.tsx
@@ -1,10 +1,12 @@
+'use client';
+
 import React from 'react';
+import { useQuery } from '@tanstack/react-query';
+import { getCompanies } from '@/lib/api';
+import CompanyRow from '@/app/components/company-row';
 
 
-
-export interface CompanyTableProps {
-    children?: React.ReactNode;
-}
+export interface CompanyTableProps {}
 const headers = [
     'Category',
     'Company',
@@ -14,7 +16,12 @@ const headers = [
     'Joined Date',
 ];
 
-function CompanyTable({ children }: CompanyTableProps) {
+function CompanyTable({  }: CompanyTableProps) {
+    const { data } = useQuery({
+        queryKey: ['companies'],
+        queryFn: () => getCompanies(),
+        staleTime: 10 * 1000,
+    });
     return (
         <div className='py-8 px-10 bg-gray-100'>
             <table className='table-auto w-full border-separate border-spacing-y-2'>
@@ -25,7 +32,11 @@ function CompanyTable({ children }: CompanyTableProps) {
                     ))}
                 </tr>
                 </thead>
-                <tbody>{children}</tbody>
+                <tbody>
+                {data?.map((company) => (
+                    <CompanyRow key={company.id} company={company} />
+                ))}
+                </tbody>
             </table>
         </div>
     );
